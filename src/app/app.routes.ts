1981d67dd93e6b96bb5b568e835832520d82dd90
Mohammad Javed <mohammadjaved@Mohammads-MacBook-Pro.local>
2024-09-18 01:04:01 +0530

    Coming soon landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DocsComponent } from './pages/docs/docs.component';
 import { AlertComponent } from './pages/docs/alert/alert.component';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent,
+        component: ComingSoonComponent,
         pathMatch: 'full'
-    },
-    {
-        path: 'docs',
-        component: DocsComponent,
-        children: [
-            {
-                path: 'alert',
-                component: AlertComponent,
-            }
-        ]
     }
 ];
---
+++ src/app/components/coming-soon/coming-soon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coming-soon',
+  standalone: true,
+  imports: [],
+  templateUrl: './coming-soon.component.html'
+})
+export class ComingSoonComponent {
+
+}
